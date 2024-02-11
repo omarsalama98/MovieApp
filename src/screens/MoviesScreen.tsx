f061143ea1f14352c6c9f1cc267709453c7506f4
omarsalama98 <osalama710@gmail.com>
2024-02-11 23:48:42 +0200

    Used native module for Android fetch

--- src/screens/MoviesScreen.tsx
+++ src/screens/MoviesScreen.tsx
@@ -1,6 +1,17 @@
 import React, {useState, useEffect} from 'react';
-import {FlatList, Image, SafeAreaView, Text} from 'react-native';
-import {Movie, MoviesScreenNavigationProp} from '../utils/Types';
+import {
+  FlatList,
+  Image,
+  NativeModules,
+  Platform,
+  SafeAreaView,
+  Text,
+} from 'react-native';
+import {
+  Movie,
+  MoviesResponse,
+  MoviesScreenNavigationProp,
+} from '../utils/Types';
 import {MoviesScreenStyles as styles} from '../styles/Styles';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
@@ -12,21 +23,36 @@ export default function MoviesScreen({navigation}: MoviesScreenNavigationProp) {
   }, []);
 
   const fetchMovies = () => {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmQzYTkzNDk3Y2M4ZjYwMmRlYmMwM2E1YWRkYTI3NiIsInN1YiI6IjY1YzM4Nzc2OGUyZTAwMDE4M2E2NWViOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DimQyXeLyO7eRW4M-2NuT2xZHdzt7gEokjOPaMX3t7w',
-      },
-    };
+    if (Platform.OS === 'android') {
+      const {APIModule} = NativeModules;
+      APIModule.nativeFetch(
+        'https://api.themoviedb.org/3/discover/movie',
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmQzYTkzNDk3Y2M4ZjYwMmRlYmMwM2E1YWRkYTI3NiIsInN1YiI6IjY1YzM4Nzc2OGUyZTAwMDE4M2E2NWViOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DimQyXeLyO7eRW4M-2NuT2xZHdzt7gEokjOPaMX3t7w',
+        (response: any) => {
+          try {
+            const response_object: MoviesResponse = JSON.parse(response);
+            setMovies(response_object.results);
+          } catch (error) {
+            return error?.toString() ?? 'Error fetching movies';
+          }
+        },
+      );
+    } else {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmQzYTkzNDk3Y2M4ZjYwMmRlYmMwM2E1YWRkYTI3NiIsInN1YiI6IjY1YzM4Nzc2OGUyZTAwMDE4M2E2NWViOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DimQyXeLyO7eRW4M-2NuT2xZHdzt7gEokjOPaMX3t7w',
+        },
+      };
 
-    fetch('https://api.themoviedb.org/3/discover/movie', options)
-      .then(response => response.json())
-      .then(response => setMovies(response.results))
-      .catch(err => console.error(err));
+      fetch('https://api.themoviedb.org/3/discover/movie', options)
+        .then(response => response.json())
+        .then(response => setMovies(response.results))
+        .catch(err => console.error(err));
+    }
   };
-  console.log(movies?.at(0)?.backdrop_path);
 
   return (
     <SafeAreaView>
